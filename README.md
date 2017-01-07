f8d25a2491e116f737023a507c8fc9e40a33d50d
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-01-07 17:52:02 -0600

    Created my application using this forked repo. Added onto the html, js, and README files. Also created a CSS file.

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Weekend 1 Challenge Starter Repo
 
-If you want, you can start your project by forking and cloning this repo.
+##Author
 
-However, feel free to start from scratch,too.
+* *Hue Vang*
 
-**REMEMBER:** Carefully read the instructions in the assignment app.
+###Details
+
+In this challenge I created an application that recorded data that was entered into the fields. This application can also add salaries up to determine how much you are spending each month on expenditures.
+
+###Hard Mode
+
+I created a delete button that removed an employee's records from the DOM, but did not change the monthly salary expenditures.
+
+###Pro Mode
+I used the data() method to link an employee's annual salary into a div and used that data to recalculate the monthly salary expenditure after an employee was removed.
+
+####Extras
+I styled the layout of the list and form fields using CSS.
+
+#####Noticeable bugs
+1. If a field has too much text, the text would overlap with the next field when appended to the DOM.
+2. If you did not enter a first name for an employee, there is no way of deleting them off the list.
+3. If two or more employees have the same first name, the 'delete' button will delete the employee who was created the earliest and so forth.
+4. My app creates a div ID using the employee's first name, so employees with the same first name would also have the same div ID.
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(function () {
   console.log('document is ready');
 
+  salaryArray = [0];
+
   $('form').on('submit', function (event) {
     console.log('form submitted');
 
@@ -13,19 +15,75 @@ $(function () {
       formData[input.name] = input.value;
     });
 
+    console.log(formData);
     appendDom(formData);
 
     clearForm();
   });
+
+  $(':button').on('click', function(event) {
+    console.log('Deleted'); //To make sure this button is working when clicked.
+
+    event.preventDefault();
+
+    var name = prompt('Please enter the employee\'s FIRST NAME');
+
+    console.log(name); //To make sure the 'name' variable is being set correctly when entered in the prompt.
+
+    if (name != null && name != '') {
+      if ($('.employee').is('#' + name)) {
+        //console.log($('#' + name + '').data('annualSalary'))
+        editExp($('#' + name + '').data("annualSalary"));
+        $('#' + name + '').remove('div');
+        alert(name + ', has been deleted from the list.');
+      }
+      else {
+        alert('Sorry, that employee is not on the list.');
+      }
+    }
+  });
 });
 
+
+
 function appendDom(emp) {
-  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  if (emp.employeeFirstName != '' || emp.employeeLastName != '' || emp.employeeIdNumber != '' || emp.employeeJobTitle != '' || emp.annualSalary != '') {
+    var $emp = $('<div id="' + emp.employeeFirstName + '"class="employee"></div>').data('annualSalary', Number(emp.annualSalary)); // create a div jQuery object
 
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+    $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
+    $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+    $emp.append('<p>' + emp.employeeJobTitle + '</p>');
+    $emp.append('<p>' + emp.annualSalary + '</p>');
 
-  $('#employees').append($emp); // append our div to the DOM
+    $('#employees').append($emp); // append our div to the DOM
+
+    if (isNaN(Number(emp.annualSalary)) == false) {
+      appendExp(emp);
+    }
+  }
+}
+
+function appendExp(exp) {
+  $('div').remove('.expenditure');
+  var $exp = $('<div class="expenditure"></div>');
+  var totalSalary = salaryArray[0];
+
+
+  $exp.append('<p>' + ((totalSalary + Number(exp.annualSalary)) / 12) + '</p>');
+
+  $('#expenditures').append($exp);
+  salaryArray[0] = totalSalary + Number(exp.annualSalary);
+}
+
+function editExp(arg) {
+  $('div').remove('.expenditure');
+  var $arg = $('<div class="expenditure"></div>');
+  var totalSalary = salaryArray[0];
+
+  $arg.append('<p>' + ((totalSalary - arg) / 12) + '</p>');
+  $('#expenditures').append($arg);
+  salaryArray[0] = totalSalary - arg;
+  //console.log(salaryArray[0]);
 }
 
 function clearForm() {
